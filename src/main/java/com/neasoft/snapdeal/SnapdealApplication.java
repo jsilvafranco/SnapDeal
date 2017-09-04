9cbe77254ab7007f51f83dd41655f94d67eb1a5e
Jairo Silva <jairo.silva@boomi.com>
2017-09-04 16:49:46 -0500

    initial commit SnapDeal reimaging promo's experience

---
+++ src/main/java/com/neasoft/snapdeal/SnapdealApplication.java
@@ -0,0 +1,12 @@
+package com.neasoft.snapdeal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SnapdealApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SnapdealApplication.class, args);
+	}
+}
---
+++ src/test/java/com/neasoft/snapdeal/SnapdealApplicationTests.java
@@ -0,0 +1,16 @@
+package com.neasoft.snapdeal;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SnapdealApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
